914d310b91d44b90b91e555dba42994d2cc9d5ce
Vinam Arora <vinam@posteo.de>
2022-10-27 09:49:22 -0400

    Adds documentation

--- gnn_neo/nn/qat/utils.py
+++ gnn_neo/nn/qat/utils.py
@@ -1,6 +1,13 @@
 from gnn_neo.quantization.qconfig import QConfig
 
+
 def _get_qconfig(kwargs):
+    """Check existence of kwargs['qconfig'] in kwargs and return its value.
+    The functional also deleted kwargs['qconfig'] from kwargs.
+    
+    :param kwargs: A kwarg dictionary
+    :return: kwargs['qconfig']
+    """
     assert 'qconfig' in kwargs
     assert isinstance(kwargs['qconfig'], QConfig)
     ret = kwargs['qconfig']
